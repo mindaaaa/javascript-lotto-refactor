8082a27ac88c659c49c03740373591a45e4879ad
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-10 18:07:54 +0900

    feat(LottoManager): 출력에 필요한 데이터를 반환하는 LottoManager 초안 설계

--- src/domain/LottoManager.js
+++ src/domain/LottoManager.js
@@ -1,3 +1,56 @@
-class LottoManager {}
+import LottoMachine from './LottoMachine.js';
+import MatchingLotto from './MatchingLotto.js';
+import ProfitCalculator from './ProfitCalculator.js';
 
+class LottoManager {
+  #purchaseAmount;
+  #ticketCount;
+  #winningNumbers;
+  #bonusNumbers;
+
+  constructor({ purchaseAmount, ticketCount, winningNumbers, bonusNumber }) {
+    this.#purchaseAmount = purchaseAmount;
+    this.#ticketCount = ticketCount;
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumbers = bonusNumber;
+  }
+  release() {
+    const machine = new LottoMachine(this.#ticketCount);
+    // 로또 발행
+    const tickets = machine.issue();
+
+    const params = {
+      winningNumbers: this.#winningNumbers,
+      bonusNumbers: this.#bonusNumbers,
+      tickets,
+    };
+
+    // 로또 맞추기
+    const matchingLotto = new MatchingLotto(params);
+    const reward = matchingLotto.match();
+
+    // 수익률 계산
+    const profitCalculator = new ProfitCalculator(this.#purchaseAmount, reward);
+    const profit = profitCalculator.calculate();
+
+    // 필요한 데이터 반환
+    return { tickets, reward, profit };
+  }
+}
+
+// tickets
+//[ Lotto {}, Lotto {}, Lotto {}, Lotto {} ]
+
+// reward
+// [
+//     { rank: 6, count: 0, prize: 2000000000 },
+//     { rank: 7, count: 1, prize: 30000000 },
+//     { rank: 5, count: 1, prize: 1500000 },
+//     { rank: 4, count: 0, prize: 50000 },
+//     { rank: 3, count: 1, prize: 5000 },
+//     { rank: 0, count: 3, prize: 0 }
+//   ]
+
+// profit
+// 121.3
 export default LottoManager;
