937ba3902cb72472fa15914db6158ace7a1464cc
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-10 16:36:21 +0900

    feat(ProfitCalculator): 수익률을 계산하는 클래스 구현

--- src/domain/ProfitCalculator.js
+++ src/domain/ProfitCalculator.js
@@ -1,3 +1,35 @@
-class ProfitCalculator {}
+class ProfitCalculator {
+  #purchaseAmount;
+  #prizeObject;
+
+  constructor(purchaseAmount, prizeObject) {
+    this.#purchaseAmount = purchaseAmount;
+    this.#prizeObject = prizeObject;
+  }
+
+  calculate() {
+    const cash = this.#prizeObject.map((prize) => {
+      const obj = this.filterObjectByKeys(prize, ['count', 'prize']);
+      Object.values(obj).reduce((acc, curr) => acc * curr, 1);
+    });
+
+    const totalCash = cash.reduce((acc, curr) => acc + curr, 0);
+    const profit = this.#profit(totalCash, this.#purchaseAmount);
+    return Math.round(profit * 10) / 100;
+  }
+
+  filterObjectByKeys = (prizeObject, keys) => {
+    return Object.keys(prizeObject)
+      .filter((key) => keys.includes(key))
+      .reduce((acc, key) => {
+        acc[key] = prizeObject[key];
+        return acc;
+      }, {});
+  };
+
+  #profit(totalCash, purchaseAmount) {
+    return ((totalCash - purchaseAmount) / purchaseAmount) * 100;
+  }
+}
 
 export default ProfitCalculator;
